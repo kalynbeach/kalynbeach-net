86748707eedb5a87d1d97d8c92c2da121a6d6b5c
Kalyn Beach <kalynbeach@gmail.com>
2025-01-18 00:53:04 -0800

    improve chroma component styles

--- components/sound/chroma.tsx
+++ components/sound/chroma.tsx
@@ -2,13 +2,30 @@ type ChromaProps = {
   data: number[];
 };
 
+const PITCH_CLASSES = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
+
 export default function Chroma({ data }: ChromaProps) {
   return (
     <div className="chroma w-full h-32 border border-primary">
       {data && data.length > 0 && (
         <div className="chroma-container w-full h-full flex flex-row items-end justify-evenly">
           {data.map((chroma, i) => (
-            <div key={i} className="chroma-bar w-full bg-primary" style={{ height: `${chroma * 100}%` }} />
+            <div key={i} className="chroma-bar w-full bg-primary flex flex-col justify-end" style={{ height: `${chroma * 100}%` }}>
+              <div className="chroma-label text-sm font-mono font-medium text-center text-primary-foreground">{PITCH_CLASSES[i]}</div>
+            </div>
           ))}
         </div>
       )}
